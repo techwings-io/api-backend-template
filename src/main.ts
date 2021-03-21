5846fecf5d757f164c68f94622ebf4987bc12d68
Marco Tedone <marco@techwings.io>
2021-03-21 11:52:08 +0000

    initial template with JWT and config enabled

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: true,
+    })
+  );
+  const port = process.env.PORT || configService.get('PORT');
+
+  await app.listen(port);
+  console.log(`Server listening on port ${port}`);
 }
 bootstrap();
